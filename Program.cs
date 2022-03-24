1880411b14e1dd4f578830631d5cb5c5e57cc68e
rogerch93 <rogerinhochaves@hotmail.com>
2022-03-23 22:20:38 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿
+   Menu();
+
+static void Menu()
+{
+    Console.Clear();
+
+    Console.WriteLine("Escolha que tipo de calculo deseja fazer:");
+    Console.WriteLine("0 - Soma");
+    Console.WriteLine("1 - Subtração");
+    Console.WriteLine("2 - Multiplicação");
+    Console.WriteLine("3 - Divisão");
+    Console.WriteLine("_________________________________________");
+    Console.WriteLine("Digite o numero correspondente a opção desejada:");
+}
+
+short resultado =  short.Parse(Console.ReadLine());
+
+
+switch (resultado)
+{
+    case 0:
+        Soma();
+        break;
+    case 1:
+        Subtracao();
+        break;
+    case 2:
+        Multiplicacao();
+        break;
+    case 3:
+        Divisao();
+        break;
+    default:
+        Menu();
+        break;
+}
+
+
+static void Soma()
+{   
+    Console.Clear();
+
+    Console.WriteLine("Digite o primeiro numero: ");
+    float v1 = float.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o segundo numero: ");
+    float v2 = float.Parse(Console.ReadLine());
+    Console.WriteLine($"O resultado da soma é: {v1 + v2}");
+    Console.ReadKey();
+    Menu();
+}
+
+static void Subtracao()
+{
+    Console.Clear();
+
+    Console.WriteLine("Digite o primeiro numero: ");
+    float v1 = float.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o segundo numero: ");
+    float v2 = float.Parse(Console.ReadLine());
+    Console.WriteLine($"O resultado da subtração é: {v1 - v2}");
+    Console.ReadKey();
+    Menu();
+}
+
+static void Multiplicacao()
+{
+    Console.Clear();
+    Console.WriteLine("Digite o primeiro numero: ");
+    float v1 = float.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o segundo numero: ");
+    float v2 = float.Parse(Console.ReadLine());
+    Console.WriteLine($"O resultado da multiplicação é: {v1 * v2}");
+    Console.ReadKey();
+    Menu();
+}
+
+static void Divisao()
+{
+    Console.Clear();
+    Console.WriteLine("Digite o primeiro numero: ");
+    float v1 = float.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o segundo numero: ");
+    float v2 = float.Parse(Console.ReadLine());
+    Console.WriteLine($"O resultado da divisão é: {v1 / v2}");
+    Console.ReadKey();
+    Menu();
+}
+
